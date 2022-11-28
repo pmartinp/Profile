166952a02e9205e7cfd17bee2c515d568f884e51
pmartinp <pmartinpdev@gmail.com>
2022-11-28 14:48:07 +0100

    Formulario sin validar

--- js/custom.js
+++ js/custom.js
@@ -1,21 +1,40 @@
 "use strict"
 
 document.addEventListener("scroll", (event) => {
-  (window.scrollY > 1) ? menuToFixed() : menuToNoFixed();
+  (window.scrollY > 1) ? menuTransition() : menuNoTransition();
 });
 
-function menuToFixed(){
+function menuTransition(){
+  try {
     let menu = document.getElementById("menu");
-    let space = document.getElementById("space-before-banner");
     menu.classList.add("bg-primary");
     menu.classList.add("border-bottom");
     menu.classList.replace("py-4", "py-2");
+  } catch (error) {
+    console.log(error);
+  }
+  try {
+    let menuForm = document.getElementById("menuForm");
+    menuForm.classList.add("border-bottom");
+    menuForm.classList.replace("py-4", "py-2");
+  } catch (error) {
+    console.log(error);
+  }
 }
 
-function menuToNoFixed(){
+function menuNoTransition(){
+  try {
     let menu = document.getElementById("menu");
-    let space = document.getElementById("space-before-banner");
-    menu.classList.remove("bg-primary");
     menu.classList.remove("border-bottom");
     menu.classList.replace("py-2", "py-4");
+  } catch (error) {
+    console.log(error);
+  }
+  try {
+    let menuForm = document.getElementById("menuForm");
+    menuForm.classList.remove("border-bottom");
+    menuForm.classList.replace("py-2", "py-4");
+  } catch (error) {
+    console.log(error);
+  }
 }
